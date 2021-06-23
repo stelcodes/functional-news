3cadcde1858b91e70499697b73cc3f70f264c4b2
Stel Abrego <stel@stel.codes>
2021-06-23 14:38:35 -0400

    Change mode to html

--- src/codes/stel/functional_news/views.clj
+++ src/codes/stel/functional_news/views.clj
@@ -9,7 +9,7 @@
 
 (defn layout
   [title & content]
-  (-> (html {:lang "en"}
+  (-> (html {:lang "en", :mode :html}
             [:head [:title title] [:meta {:charset "utf-8"}] [:meta {:http-equiv "X-UA-Compatible", :content "IE=edge"}]
              [:meta {:name "viewport", :content "width=device-width, initial-scale=1.0"}]
              [:link {:rel "shortcut icon", :href "/assets/favicon.ico", :type "image/x-icon"}]
